a9da82ee14bbeef2ed19dce979c8d951467e386a
Oscar <oscar.kennedysmith>
2019-11-09 15:12:59 +1000

    Included methods to stay DRY

--- exercise.rb
+++ exercise.rb
@@ -1,3 +1,20 @@
+def get_user_input
+    puts "What's your first number?"
+    num_1 = gets.chomp.to_i
+    puts "What's your second number?"
+    num_2 = gets.chomp.to_i
+    return num_1, num_2
+end 
+
+def sum 
+    num_1, num_2 = get_user_input()
+    puts "Your first number is #{num_1}"
+    puts "Your second number is #{num_2}"
+    result = num_1 + num_2
+    puts result 
+end  
+
+
 quit = false 
 until quit do 
     puts "options"
@@ -13,14 +30,24 @@ until quit do
         when "q"
             quit = true
         when "1"
+            get_user_input()
+            sum()            
+            puts result
+        when "2"
+            get_user_input()
+            result = num_1 - num_2 
+            puts result
+        when "3"
             puts "What's your first number?"
             num_1 = gets.chomp.to_i
             puts "What's your second number?"
             num_2 = gets.chomp.to_i
-            result = num_1 + num_2 
+            result = num_1 / num_2 
             puts result
         end 
 end 
+
+    
 
 
 
